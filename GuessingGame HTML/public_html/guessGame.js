b25b977c4269a1ef6093c7f15ca0d66454f282e7
Paul Trindall <ptrindall048@students.ccsnh.edu>
2017-10-09 13:14:59 -0400

    updated
    Working on getting my variable to hold values.

--- GuessingGame HTML/public_html/guessGame.js
+++ GuessingGame HTML/public_html/guessGame.js
@@ -1,23 +1,17 @@
-var num;
-var guess;
-function number(){
-	num = parseInt(document.getElementById("numIn").value);
-	//alert(num);
-}
+
 function guess(){
-	guess = parseInt(document.getElementById("input").value);
-	alert(num);
-	if(guess == num){
+	var num = parseInt(document.getElementById("numIn").value);
+	var guess = parseInt(document.getElementById("inputguess").value);
+	if(guess = num){
 		//have the program send back correct
-		document.getElementById("anw").innerHTML = "<p> You got it right! <p>";
-		return play(false);
+		document.getElementById("inputfield").placeholder = 'Youre right!!';
 	}
 	if(guess > num){
 		//send back to high
-		document.getElementById("anw").innerHTML = "<p> Your guess is to high <p>";
+		document.getElementById("inputfield").placeholder = 'Your guess is to high';
 	}
 	if(guess < num){
 		//send back to low
-		document.getElementById("anw").innerHTML = "<p> Your guess is to low <p>";
+		document.getElementById("inputfield").placeholder = 'Your guess is to low';
 	}
 }
