1596633c9fb43b2f76cf3fda3630e61ff98a8bd1
Paul Trindall <ptrindall048@students.ccsnh.edu>
2017-10-09 14:02:33 -0400

    got the javascript to work
    javascript works now but the user has to press back in the input to see if they were right, to high or to low

--- GuessingGame HTML/public_html/guessGame.js
+++ GuessingGame HTML/public_html/guessGame.js
@@ -1,17 +1,23 @@
-
+var num;
+var guess;
+function number(){
+	num = parseInt(document.getElementById("numIn").value);
+	//alert(num);
+}
 function guess(){
-	var num = parseInt(document.getElementById("numIn").value);
-	var guess = parseInt(document.getElementById("inputguess").value);
-	if(guess = num){
+	num = parseInt(document.getElementById("numIn").value);
+	guess = parseInt(document.getElementById("inputguess").value);
+	
+	if(guess == num){
 		//have the program send back correct
-		document.getElementById("inputfield").placeholder = 'Youre right!!';
+		document.getElementById("inputguess").placeholder = "You're right!!";
 	}
 	if(guess > num){
 		//send back to high
-		document.getElementById("inputfield").placeholder = 'Your guess is to high';
+		document.getElementById("inputguess").placeholder = "Your guess is to high";
 	}
 	if(guess < num){
 		//send back to low
-		document.getElementById("inputfield").placeholder = 'Your guess is to low';
+		document.getElementById("inputguess").placeholder = "Your guess is to low";
 	}
 }
