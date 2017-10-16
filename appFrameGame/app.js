40032a71386cebeaaf2e8bec7e6f5a36af762cd0
Paul Trindall <ptrindall048@students.ccsnh.edu>
2017-10-16 15:47:33 -0400

    game should work

--- appFrameGame/app.js
+++ appFrameGame/app.js
@@ -1,34 +1,4 @@
 "use strict";
-/*
-// using a function contructor form to create an object
-var app window;
-var num;
-var guess;
-
-function number(){
-	num = parseInt(document.getElementById("numIn").value);
-        document.getElementById("numIn").value = "";
-}
-function guess(){
-	guess = parseInt(document.getElementById("inputguess").value);
-        if(guess === num){
-            //have the program send back correct
-            document.getElementById("inputguess").value = "You're right!!";
-            //document.getElementById("inputguess").reset();
-        }
-        if(guess > num){
-	//send back to high
-            document.getElementById("inputguess").value = "Your guess is to high";
-            //document.getElementById("inputguess").reset();
-        }
-        if(guess < num){
-            //send back to low
-            document.getElementById("inputguess").value = "Your guess is to low";
-            //document.getElementById("inputguess").reset();
-        }
-}
-*/
-//functions that load the page and resources
 function MyApp()
 {
 	var version = "v1.0";
@@ -55,7 +25,7 @@ function MyApp()
 	function number(){
 	num = parseInt(document.getElementById("numIn").value);
 		//$("numIn").val() = "";
-        document.getElementById("numIn").val = "";
+        document.getElementById("numIn").value = "";
 	}
 	function guess(){
 		guess = parseInt(document.getElementById("inputguess").value);
